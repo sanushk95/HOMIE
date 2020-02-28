3a61eb13a5b94984540c4a0b5a045822d08cabe6
Saniya Shaikh <57577995+sanushk95@users.noreply.github.com>
2020-02-28 16:50:18 -0500

    Update main.js
    Added function for motion

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 let socket=io.connect("http://localhost:3000");
 let therm= document.querySelector("#thermometer");
 let button=document.querySelector("#light_button");
+let motion=document.querySelector('#motion_toggle');
 
 socket.on('temperature',function(temperature){
 	console.log(temperature);
@@ -9,5 +10,9 @@ socket.on('temperature',function(temperature){
 function toggleLight(){
 	socket.emit('light_status');
 	console.log("emit light")
+}
+function readMotion(){
+	socket.emit('motion_status');
+	console.log("motion");
 }
 button.addEventListener("click",toggleLight);
