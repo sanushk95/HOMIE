ffed9932cc121147685707c5c8cf6561e079935a
Sohana Kadiwala <sohakadiwala@gmail.com>
2020-03-30 18:20:10 -0400

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,18 +1,28 @@
-let socket=io.connect("http://localhost:3000");
-let therm= document.querySelector("#thermometer");
-let button=document.querySelector("#light_button");
-let motion=document.querySelector('#motion_toggle');
+(function () {
 
-socket.on('temperature',function(temperature){
-	console.log(temperature);
-	therm.innerHTML=temperature;
-});
-function toggleLight(){
-	socket.emit('light_status');
-	console.log("emit light")
-}
-function readMotion(){
-	socket.emit('motion_status');
-	console.log("motion");
-}
-button.addEventListener("click",toggleLight);
+	let socket=io.connect("http://localhost:3000");
+	let motionElement= document.querySelector("#motionElement");
+	let thermElement= document.querySelector("#thermElement");
+	//let button=document.querySelector("#light_button");
+	
+	socket.on("motion",function(e){
+		console.log();
+		if(e){
+			motionElement.innerHTML='Motion Active';
+		}else{
+			motionElement.innerHTML='Motion InActive';
+		}
+		
+	});
+	
+	socket.on("multi",function(e){
+		console.log(e);
+		thermElement.innerHTML=multi;
+		if(e){
+			thermElement.innerHTML='Temperture Active';
+		}else{
+			thermElement.innerHTML='Temperture InActive';
+		}
+	});
+})();
+
